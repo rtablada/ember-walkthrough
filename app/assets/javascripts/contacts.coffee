44e3390ff0cf2b9d36592e3821d642ab812b94bc
Ryan Tablada <ryan.tablada@gmail.com>
2013-08-17 16:22:29 -0500

    Initial Ember Workflow

---
+++ app/assets/javascripts/contacts.coffee
@@ -0,0 +1,1 @@
+Contacts = Ember.Application.create();
---
+++ app/views/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Contacts</title>
+    <link rel="stylesheet" href="css/gumby.css">
+</head>
+<body>
+
+    <div class="content">
+        <div class="row">
+            <h1>My Contacts</h1>
+        </div>
+
+        <div class="row" style="padding-bottom: 20px;">
+            <div class="medium primary btn"><a href="#">Add Order</a></div>
+        </div>
+
+        <div class="row">
+            <table>
+                <thead>
+                    <th>Date</th>
+                    <th>Items</th>
+                    <th>Amount</th>
+                    <th>Remove</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Jan 01, 2013</td>
+                        <td>3</td>
+                        <td>$30</td>
+                        <td>
+                            <button class="destroy"><i class="icon-cancel"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>May 10, 2013</td>
+                        <td>Donaghy</td>
+                        <td>$50</td>
+                        <td>
+                            <button class="destroy"><i class="icon-cancel"></i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>June 20, 2013</td>
+                        <td>1</td>
+                        <td>$100</td>
+                        <td>
+                            <button class="destroy"><i class="icon-cancel"></i></button>
+                        </td>
+                    </tr>
+                    <tr class="total">
+                        <td></td>
+                        <td></td>
+                        <td style="font-weight:bold">$180</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <?= javascript_include_tag() ?>
+</body>
+</html>
